29e36829b96111e773d5fae809d5d18b05910b7f
Kirill <kirillkolesnichenko21@gmail.com>
2021-12-27 14:47:15 +0300

    first commit

---
+++ sum.pas
@@ -0,0 +1,17 @@
+﻿program gypsy;
+var input_num, even_sum, odd_sum, i:integer;
+begin
+  write('Введите число: ');
+  readln(input_num);
+  
+  for i:= 1 to input_num do
+  begin
+    if i mod 2 = 0 then
+        even_sum:= even_sum + i
+      else
+        odd_sum:= odd_sum + i;
+  end;
+  
+  writeln('Сумма четных чисел: ' + even_sum);
+  writeln('Сумма нечетных чисел: ' + odd_sum);
+end.
